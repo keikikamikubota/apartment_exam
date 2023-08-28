12835c013f7f5863e889e0a152b77342e5564a19
keiki <kkeki0410@gmail.com>
2023-08-28 18:58:40 +0900

    物件登録時物件名だけは入力するようなバリデーションを追記した

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
+  validates :name, presence: true
   has_many :stations, dependent: :destroy
   accepts_nested_attributes_for :stations,
                                 allow_destroy: true,
